adc51ab7581ed1ccc18b1d3782f041912003f076
bernardo <bernardo_nf@hotmail.com>
2024-07-16 15:51:17 -0300

    update script

--- script.js
+++ script.js
@@ -1,80 +1,191 @@
-// Função que irá salvar a atividade
-function saveActivity(event) {
-  // Previne o comportamento padrão do formulário de recarregar a página
-  event.preventDefault()
+// bibliotecas e códigos de terceiros
+const formatador = (data) => {
+  return {
+    dia: {
+      numerico: dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd')
+      }
+    },
+    mes: dayjs(data).format('MMMM'),
+    hora: dayjs(data).format('HH:mm')
+  }
+}
 
-  // Pega os dados do formulário
-  const form = new FormData(event.target)
-  const activity = form.get('activity')
-  const day = form.get('day')
-  const time = form.get('time')
+// object
+const atividade = {
+  nome: 'Almoço',
+  data: new Date('2024-07-11 10:00'),
+  finalizada: true
+}
 
-  // Gera um ID único para a nova atividade
-  const id = `${activity}-${day}-${time}`
 
-  // Cria um objeto com os dados da atividade
-  const activityData = {
-    id,
-    activity,
-    day,
-    time
+// array
+let atividades = [
+  atividade, 
+  {
+    nome: 'Academia em grupo',
+    data: new Date('2024-07-09 12:00'),
+    finalizada: false
+  },
+  {
+    nome: 'Gaming session',
+    data: new Date('2024-07-09 16:00'),
+    finalizada: true
+  }
+]
+
+/*atividades = []*/
+
+
+const criarItemDeAtividade = (atividade) => {
+  let input = `
+  <input 
+  onchange="concluirAtividade(event)"
+  value="${atividade.data}"
+  type="checkbox" 
+  `
+
+  if(atividade.finalizada) {
+    input += 'checked'
   }
 
-  // Salva os dados no localStorage
-  saveToLocalStorage(activityData)
+  input += '>'
 
-  // Adiciona a atividade na lista
-  addActivityToList(activityData)
+  const formatar = formatador(atividade.data)
 
-  // Limpa o formulário
-  event.target.reset()
+  return `
+  <div class="card-bg">
+    ${input}
+
+    <div>
+      <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.50008 10L9.16675 11.6667L12.5001 8.33335M18.3334 10C18.3334 14.6024 14.6025 18.3334 10.0001 18.3334C5.39771 18.3334 1.66675 14.6024 1.66675 10C1.66675 5.39765 5.39771 1.66669 10.0001 1.66669C14.6025 1.66669 18.3334 5.39765 18.3334 10Z" stroke="#BEF264" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+
+      <svg class="inactive" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.41664 1.81836C9.46249 1.61597 10.5374 1.61597 11.5833 1.81836M11.5833 18.1817C10.5374 18.3841 9.46249 18.3841 8.41664 18.1817M14.6741 3.10086C15.5587 3.70022 16.3197 4.46409 16.9158 5.35086M1.8183 11.5834C1.6159 10.5375 1.6159 9.46255 1.8183 8.4167M16.8991 14.6742C16.2998 15.5588 15.5359 16.3198 14.6491 16.9159M18.1816 8.4167C18.384 9.46255 18.384 10.5375 18.1816 11.5834M3.1008 5.32586C3.70016 4.44131 4.46403 3.68026 5.3508 3.0842M5.3258 16.8992C4.44124 16.2998 3.6802 15.536 3.08414 14.6492" stroke="#A1A1AA" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+
+      <span>${atividade.nome}</span>
+    </div>
+
+    <time class="short">
+      ${formatar.dia.semana.curto}.
+      ${formatar.dia.numerico} <br>
+      ${formatar.hora}
+    </time>
+
+    <time class="full">
+      ${formatar.dia.semana.longo}, 
+      dia ${formatar.dia.numerico}
+      de ${formatar.mes} 
+      às ${formatar.hora}h 
+    </time>
+  </div>
+  `
 }
 
-// Função que irá salvar os dados no localStorage
-function saveToLocalStorage(activityData) {
-  // Pega as atividades do localStorage
-  const activities = JSON.parse(localStorage.getItem('activities')) || []
 
-  // Adiciona a nova atividade
-  activities.push(activityData)
+  const atualizarListaDeAtividades = () => {
+    const section = document.querySelector('section')
+    section.innerHTML = ''
+    
+    // verificar se a minha lista (array) está vazia
+    if (atividades.length == 0) {
+      section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`
+      return
+    }
 
-  // Salva de volta no localStorage
-  localStorage.setItem('activities', JSON.stringify(activities))
+    for(let atividade of atividades) {
+      section.innerHTML += criarItemDeAtividade(atividade)
+    }
 }
 
-// Função que irá adicionar a atividade na lista
-function addActivityToList(activityData) {
-  // Criando os elementos
-  const section = document.querySelector('main section')
-  const card = document.createElement('div')
-  const cardTime = document.createElement('div')
-  const cardActivity = document.createElement('div')
+atualizarListaDeAtividades()
 
-  // Adicionando as classes
-  card.classList.add('card')
-  cardTime.classList.add('card-time')
-  cardActivity.classList.add('card-activity')
+const salvarAtividade = (event) => {
+  event.preventDefault()
+  const dadosDoFormulario = new FormData(event.target)
 
-  // Adicionando os dados
-  cardTime.textContent = `${activityData.day} - ${activityData.time}`
-  cardActivity.textContent = activityData.activity
+  const nome = dadosDoFormulario.get('atividade')
+  const dia = dadosDoFormulario.get('dia')
+  const hora = dadosDoFormulario.get('hora')
+  const data = `${dia} ${hora}`
 
-  // Adicionando os elementos no card
-  card.appendChild(cardTime)
-  card.appendChild(cardActivity)
+  const novaAtividade = {
+    nome,
+    data,
+    finalizada: false
+  }
 
-  // Adicionando o card na seção
-  section.appendChild(card)
+  const atividadeExiste = atividades.find((atividade) => {
+    return atividade.data == novaAtividade.data
+  })
+
+  if (atividadeExiste) {
+    return alert('Dia/Hora não disponível!')
+  }
+
+  atividades = [novaAtividade, ...atividades]
+  atualizarListaDeAtividades()
 }
 
-// Função que irá carregar as atividades do localStorage
-function loadActivities() {
-  // Pega as atividades do localStorage
-  const activities = JSON.parse(localStorage.getItem('activities')) || []
+const criarDiasSelecao = () => {
+  const dias = [
+    '2024-02-28',
+    '2024-02-29',
+    '2024-03-01',
+    '2024-03-02',
+    '2024-03-03 '
+  ]
 
-  // Adicionando cada atividade na lista
-  activities.forEach(addActivityToList)
+  let diasSelecao = ''
+
+  for (let dia of dias) {
+    const formatar = formatador(dia)
+    const diaFormatado = `
+      ${formatar.dia.numerico} de
+      ${formatar.mes}
+    `
+    diasSelecao += `
+      <option value="${dia}">${diaFormatado}</option>
+    `
+  }
+  document.querySelector('select[name="dia"]').innerHTML = diasSelecao
 }
+criarDiasSelecao()
 
-// Carregando as atividades quando a página carrega
-window.onload = loadActivities
+
+const criarHorasSelecao = () => {
+  let horasDisponiveis = ''
+
+  for (let i = 6; i < 23; i++) {
+    const hora = String(i).padStart(2, '0')
+    horasDisponiveis += `
+      <option value="${hora}:00">${hora}:00</option>
+    `
+    horasDisponiveis += `
+      <option value="${hora}:30">${hora}:30</option>
+    `
+  }
+
+  document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis
+}
+criarHorasSelecao()
+
+const concluirAtividade = (event) => {
+  const input = event.target
+  const dataDesteInput = input.value
+
+  const atividade = atividades.find((atividade) => {
+    return atividade.data == dataDesteInput
+  })
+
+  if (!atividade) {
+    return
+  }
+
+  atividade.finalizada = !atividade.finalizada
+}
